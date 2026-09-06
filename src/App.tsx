076d0c040677cbf31f16150efecfe4adda9d0ee8
javimed <javiermedmata@gmail.com>
2026-09-06 12:06:12 +0200

    feat: navegación y enrutamiento para videojuegos (issue #33)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import HomePage from './pages/HomePage'
 import BookListPage from './pages/BookListPage'
 import BookCreatePage from './pages/BookCreatePage'
 import BookEditPage from './pages/BookEditPage'
+import GameListPage from './pages/GameListPage'
+import GameCreatePage from './pages/GameCreatePage'
+import GameEditPage from './pages/GameEditPage'
 
 export default function App() {
   return (
@@ -15,6 +18,9 @@ export default function App() {
           <Route path="/coleccion" element={<BookListPage />} />
           <Route path="/nuevo" element={<BookCreatePage />} />
           <Route path="/editar/:id" element={<BookEditPage />} />
+          <Route path="/juegos" element={<GameListPage />} />
+          <Route path="/juegos/nuevo" element={<GameCreatePage />} />
+          <Route path="/juegos/editar/:id" element={<GameEditPage />} />
         </Routes>
       </main>
     </BrowserRouter>
